9d4231b7f6ca29af7ac0c6092d5a2f2e3d2ff99e
Fatih Saydam <51199899+fatihsaydam@users.noreply.github.com>
2019-06-19 19:18:44 +0300

    Add files via upload

---
+++ CategoryController.cs
@@ -0,0 +1,65 @@
+﻿using EntityFrameworkModelUygulamasi.Models.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntityFrameworkModelUygulamasi.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        NorthwindEntities db = new NorthwindEntities();
+        public ActionResult IndexCategory()
+        {
+            var model = db.Categories.ToList();
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult NewCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult NewCategory(Categories category)
+        {
+            db.Categories.Add(category);
+            db.SaveChanges();
+            return RedirectToAction("IndexCategory","Category");
+        }
+
+        public ActionResult UpdateCategory(Categories category)
+        {
+            var guncellenecekKategori = db.Categories.Find(category.CategoryID);
+            guncellenecekKategori.CategoryName = category.CategoryName;
+            guncellenecekKategori.Description = category.Description;            
+            db.SaveChanges();
+            return RedirectToAction("IndexCategory", "Category");
+        }
+
+        public ActionResult Guncelle(int id)
+        {
+            var model = db.Categories.Find(id);
+            if (model == null)
+            {
+                return HttpNotFound();
+            }
+            return View("UpdateCategory", model);
+        }
+
+        public ActionResult Sil(int id)
+        {
+            var silinecekKategori = db.Categories.Find(id);
+            if (silinecekKategori==null)
+            {
+                return HttpNotFound();
+            }
+            db.Categories.Remove(silinecekKategori);
+            db.SaveChanges();
+            return RedirectToAction("IndexCategory", "Category");
+        }
+    }
+}
---
+++ ProductController.cs
@@ -0,0 +1,66 @@
+﻿using EntityFrameworkModelUygulamasi.Models.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntityFrameworkModelUygulamasi.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        NorthwindEntities db = new NorthwindEntities();
+        public ActionResult IndexProduct()
+        {
+            var model = db.Products.ToList();
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult NewProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult NewProduct(Products product)
+        {
+            db.Products.Add(product);
+            db.SaveChanges();
+            return RedirectToAction("IndexProduct","Product");
+        }
+
+        public ActionResult UpdateProduct(Products product)
+        {
+            var guncellenecekUrun = db.Products.Find(product.ProductID);
+            guncellenecekUrun.ProductName = product.ProductName;
+            guncellenecekUrun.UnitPrice = product.UnitPrice;
+            guncellenecekUrun.UnitsInStock = product.UnitsInStock;
+            db.SaveChanges();
+            return RedirectToAction("IndexProduct", "Product");
+        }
+
+        public ActionResult Guncelle(int id)
+        {
+            var model = db.Products.Find(id);
+            if (model == null)
+            {
+                return HttpNotFound();
+            }
+            return View("UpdateProduct", model);
+        }
+        
+        public ActionResult Sil(int id)
+        {
+            var model = db.Products.Find(id);
+            if (model == null)
+            {
+                return HttpNotFound();
+            }
+            db.Products.Remove(model);
+            db.SaveChanges();
+            return RedirectToAction("IndexProduct","Product");
+        }
+    }
+}
